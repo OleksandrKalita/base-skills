8d21c91830cdc9712b61caec569f3dd05127131f
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-09-18 17:30:34 +0200

    base web-project structure with webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+const HtmlWebpackplugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/scripts/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name][hash].js",
+    },
+    devServer: {
+        port: 3200,
+    },
+    plugins: [
+        new HtmlWebpackplugin({template: "./src/pages/index.html"}),
+        new MiniCssExtractPlugin({filename: "styles.[hash].css"}),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+}
