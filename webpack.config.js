fbfacb3bf418c7cc1fb67d054eeb0717961ce623
Oleksandr Kalita <olexadr.kalita@gmail.com>
2023-09-19 09:13:03 +0200

    header section

--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,25 @@ module.exports = {
     entry: "./src/scripts/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name][hash].js",
+        filename: "[name]-[hash].js",
     },
     devServer: {
         port: 3200,
     },
     plugins: [
-        new HtmlWebpackplugin({template: "./src/pages/index.html"}),
-        new MiniCssExtractPlugin({filename: "styles.[hash].css"}),
+        new HtmlWebpackplugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, "./src/pages/index.html"),
+        }),
+        new MiniCssExtractPlugin({filename: "styles-[hash].css"}),
         new CleanWebpackPlugin(),
     ],
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
             {
                 test: /\.css/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
